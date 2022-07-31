89cb3c58af39588c36759a0cb003eea3e9fa0793
Wazier <m.waziruddin@gmail.com>
2022-07-31 19:40:22 +0700

    No Input Number

--- src/app.jsx
+++ src/app.jsx
@@ -44,7 +44,9 @@ Wassallamualaikum Warahmatullahi Wabarakatuh`
   const handleAddForm = async (e) => {
     e.preventDefault();
 
-    if(payload.name.length === 0 || !payload.telp.length === 0){
+    if(payload.name.length === 0 
+      // || !payload.telp.length === 0
+      ){
       alert('Harap mengisi Data terlebih dahulu');
       return;
     }
@@ -53,10 +55,11 @@ Wassallamualaikum Warahmatullahi Wabarakatuh`
       id: Math.floor(100000 + Math.random() * 900000),
       name: payload.name,
       link: WEDDING_URL + payload.name,
-      telp: convertPhoneNumberWithoutPlus(payload.telp),
+      // telp: convertPhoneNumberWithoutPlus(payload.telp),
       status: false,
       message: templateFormData(payload.name),
-      whatsappLink: `https://wa.me/${convertPhoneNumberWithoutPlus(payload.telp)}?text=${encodeURIComponent(templateFormData(payload.name))}`
+      // whatsappLink: `https://wa.me/${convertPhoneNumberWithoutPlus(payload.telp)}?text=${encodeURIComponent(templateFormData(payload.name))}`
+      whatsappLink: `whatsapp://send?text=${encodeURIComponent(templateFormData(payload.name))}`
     };
 
     console.log(req.whatsappLink)
@@ -93,12 +96,12 @@ Wassallamualaikum Warahmatullahi Wabarakatuh`
           </label>
           <input value={payload.name} onChange={(e) => setPayload({...payload, [e.target.name]: e.target.value})} name="name" autoFocus className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Nama (Contoh: Yazied)" />
         </div>
-        <div className=''>
+        {/* <div className=''>
           <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
             Telp
           </label>
           <input value={payload.telp} onChange={(e) => setPayload({...payload, [e.target.name]: e.target.value})} name="telp" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
-        </div>
+        </div> */}
 
         <div>
           <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
@@ -112,7 +115,7 @@ Wassallamualaikum Warahmatullahi Wabarakatuh`
           <thead>
             <tr>
               <th>Nama</th>
-              <th>Telp</th>
+              {/* <th>Telp</th> */}
               <th>Action</th>
             </tr>
           </thead>
@@ -123,9 +126,9 @@ Wassallamualaikum Warahmatullahi Wabarakatuh`
                   <td className='border px-7 py-2'>
                     {d.name}
                   </td>
-                  <td className='border px-7 py-2'>
+                  {/* <td className='border px-7 py-2'>
                     {d.telp}
-                  </td>
+                  </td> */}
                   <a href={d.whatsappLink} target="_blank">
                     <buttons type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                       Kirim Whatsapp
@@ -138,7 +141,7 @@ Wassallamualaikum Warahmatullahi Wabarakatuh`
           <tfoot>
             <tr>
               <th>Nama</th>
-              <th>Telp</th>
+              {/* <th>Telp</th> */}
               <th>Action</th>
             </tr>
           </tfoot>
